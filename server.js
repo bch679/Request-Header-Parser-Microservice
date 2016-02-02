916d4b0b31daa952d2d687821700eb48aab80007
Brad Hale <bradhale007@aol.com>
2016-02-02 22:11:41 +0000

    final commit

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express'),
     app = express(),
-    routes = require('./app/routes/index.js');
+    useragent = require('express-useragent');
 
-routes(app);
-
+app.use(useragent.express());
+app.get('/', function (req, res) {
+  var buffer = req.useragent;
+  res.json({'Browser': buffer.browser, 'ip address': req.headers['x-forwarded-for'], 'Language': req.headers["accept-language"], 'OS': buffer.os});
+  });
 app.listen(8080, function(){
   console.log('listening on port 8080...');
 });
